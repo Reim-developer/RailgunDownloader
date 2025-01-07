57d4da9020beb8c7e7cf2f4a930fba7974c5f26e
Reim-developer <contact.kaxtr@gmail.com>
2025-01-07 16:58:59 +0700

    Fixed UI: 'result log cannot scroll'

--- src/main/java/RailgunDownloaderV4/Components/YoutubeUI/Resultlog.java
+++ src/main/java/RailgunDownloaderV4/Components/YoutubeUI/Resultlog.java
@@ -4,14 +4,21 @@ import java.awt.*;
 
 public class Resultlog {
     public void SetResultlog(JFrame App, JTextArea ResultArea) {
-        ResultArea.setSize(new Dimension(400, 200));
         ResultArea.setFont(new Font("Consolas", Font.PLAIN, 10));
-        ResultArea.setBackground(new Color(69, 69, 69));
-        ResultArea.setLocation(new Point(100, 50));
-        ResultArea.setForeground(Color.WHITE);
         ResultArea.setEditable(false);
+        ResultArea.setWrapStyleWord(true);
+        ResultArea.setLineWrap(true);
+        ResultArea.setBackground(new Color(69, 69, 69));
+        ResultArea.setForeground(Color.WHITE);
         ResultArea.setToolTipText("Your download result will be displayed here");
 
-        App.add(ResultArea);
+        JScrollPane ScrollPanel = new JScrollPane(ResultArea);
+        ScrollPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        ScrollPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        ScrollPanel.setSize(new Dimension(400, 200));
+        ScrollPanel.setLocation(new Point(100, 50));
+        ScrollPanel.setBorder(null);
+
+        App.add(ScrollPanel);
     }
 }
