31080fd2dd07165cdb48184f11549f3e63a1f5b1
Reim-developer <contact.kaxtr@gmail.com>
2025-01-07 12:31:29 +0700

    Added UI component in YoutubeUI package 'ChoosePath.java'

---
+++ src/main/java/RailgunDownloaderV4/Components/YoutubeUI/ChoosePath.java
@@ -0,0 +1,49 @@
+/*
+ * RAILGUN DOWNLOADER VERSION 4.0.0
+ * ChoosePath.java
+ */
+package RailgunDownloaderV4.Components.YoutubeUI;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+import java.io.File;
+
+public class ChoosePath {
+    private final YoutubeUI YoutubeUIContext;
+
+    public ChoosePath(YoutubeUI YoutubeUIContext) {
+        this.YoutubeUIContext = YoutubeUIContext;
+    }
+    public void SetChoosePath(JFrame App, JButton ChoosePathButton) {
+        JFileChooser BrowserFolder = new JFileChooser();
+
+        Image ButtonIcon = Toolkit.getDefaultToolkit().getImage(
+                Objects.requireNonNull(getClass().getResource("/FolderPath.png"))
+        );
+        ChoosePathButton.setBorder(null);
+        ChoosePathButton.setLocation(new Point(100, 420));
+        ChoosePathButton.setSize(new Dimension(50, 50));
+        ChoosePathButton.setContentAreaFilled(false);
+        ChoosePathButton.setIcon(new ImageIcon(ButtonIcon.getScaledInstance(50, 50, Image.SCALE_FAST)));
+        ChoosePathButton.setBorderPainted(false);
+        ChoosePathButton.setFocusPainted(false);
+
+        ChoosePathButton.setToolTipText("Choose save path");
+
+        OpenSaveFolderPath(App, BrowserFolder, ChoosePathButton);
+        App.add(ChoosePathButton);
+    }
+
+    private void OpenSaveFolderPath(JFrame App, JFileChooser FolderBrowser, JButton ChoosePathButton) {
+        ChoosePathButton.addActionListener(e -> {
+            FolderBrowser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+            int OpenResult = FolderBrowser.showDialog(App, "Choose save path");
+
+            if(OpenResult == JFileChooser.APPROVE_OPTION) {
+                File ChoosePath = FolderBrowser.getSelectedFile();
+                YoutubeUIContext.ShowPathField.setText(ChoosePath.toString());
+            }
+        });
+    }
+}
