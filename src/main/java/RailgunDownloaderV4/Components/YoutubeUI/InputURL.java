6ff707d5a0a48beae2cd746ca117122b38c9a924
Reim-developer <contact.kaxtr@gmail.com>
2025-01-07 12:33:31 +0700

    Added UI component for package YoutubeUI 'InputURL.java'

---
+++ src/main/java/RailgunDownloaderV4/Components/YoutubeUI/InputURL.java
@@ -0,0 +1,22 @@
+/*
+ * RAILGUN DOWNLOADER - VERSION 4.0.0
+ * InputURL.java
+ */
+package RailgunDownloaderV4.Components.YoutubeUI;
+import javax.swing.*;
+import java.awt.*;
+
+public class InputURL {
+    public  void SetInputURL(JFrame App, JTextField URLInput) {
+        URLInput.setSize(new Dimension(400, 30));
+        URLInput.setBorder(null);
+        URLInput.setBackground(new Color(69, 69, 69));
+        URLInput.setForeground(Color.WHITE);
+        URLInput.setFont(new Font("Consolas", Font.PLAIN, 15));
+        URLInput.setLocation(new Point(100, 370));
+
+        URLInput.setToolTipText("Input your Youtube URL here.");
+
+        App.add(URLInput);
+    }
+}
