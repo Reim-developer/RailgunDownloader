4f9c8a6b84cc32bbe52a3ed1e9d79ee1505e0f41
Reim-developer <contact.kaxtr@gmail.com>
2025-01-07 12:24:49 +0700

    Added Application.java

---
+++ src/main/java/RailgunDownloaderV4/Application.java
@@ -0,0 +1,45 @@
+/*
+ * RAILGUN DOWNLOADER - VERSION 4.0.0
+ * Application.java
+ */
+package RailgunDownloaderV4;
+import RailgunDownloaderV4.Components.Youtube;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class Application {
+    public JFrame App;
+    private final Youtube YoutubeComponent;
+    private final JButton YoutubeButton;
+    public Application() {
+        App = new JFrame();
+        YoutubeButton = new JButton();
+        YoutubeComponent = new Youtube(this);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Application AppScene = new Application();
+            AppScene.SetProperty();
+            AppScene.YoutubeComponent.SetYoutubeButton(AppScene.YoutubeButton);
+        });
+    }
+
+    private void SetProperty() {
+        App.setTitle("Railgun Downloader V4");
+        App.setIconImage(
+               Objects.requireNonNull(
+                       Toolkit.getDefaultToolkit()
+                               .getImage(getClass().getResource("/AppIcon.jpg"))
+               )
+        );
+        App.setSize(600, 600);
+        App.setMaximumSize(new Dimension(600, 600));
+        App.setLayout(null);
+        App.setLocationRelativeTo(null);
+        App.getContentPane().setBackground(new Color(21, 21, 21));
+        App.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        App.setResizable(false);
+        App.setVisible(true);
+    }
+}
