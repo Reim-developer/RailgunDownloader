39ad9c6340859a266e6d6701924024875a583c65
Reim-developer <contact.kaxtr@gmail.com>
2025-01-08 13:08:28 +0700

    Added new component 'urlField'

--- src/main/java/railgunDownloaderV4/components/nhentaiUI/NhentaiUI.kt
+++ src/main/java/railgunDownloaderV4/components/nhentaiUI/NhentaiUI.kt
@@ -1,21 +1,38 @@
 package railgunDownloaderV4.components.nhentaiUI
 
 import railgunDownloaderV4.Application
+import railgunDownloaderV4.components.ulti.SetIcon
+import java.awt.Color
+import java.awt.Dimension
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
 import javax.swing.JFrame
+import javax.swing.JTextField
+import javax.swing.SwingUtilities
 import javax.swing.WindowConstants
 
 class NhentaiUI (private val appScene: Application){
+    private val setIconComponent: SetIcon by lazy { SetIcon() }
+    private val urlFieldComponent: URLField by lazy { URLField() }
+
+    private val urlField: JTextField by lazy { JTextField() }
 
     fun showNhentaiUI(visible: Boolean = false) {
         val nhentaiUI = JFrame()
-        nhentaiUI.title = "Download from Nhentai.net"
-        nhentaiUI.setSize(600, 600)
-        nhentaiUI.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
-        nhentaiUI.setLocationRelativeTo(null)
-        nhentaiUI.isVisible = visible
+        SwingUtilities.invokeLater {
+            nhentaiUI.title = "Download from Nhentai.net"
+            nhentaiUI.setSize(600, 600)
+            nhentaiUI.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
+            nhentaiUI.setLocationRelativeTo(null)
+            nhentaiUI.isResizable = false
+            nhentaiUI.maximumSize = Dimension(600, 600)
+            nhentaiUI.contentPane.background = Color(21, 21, 21)
+            nhentaiUI.iconImage = setIconComponent.AppIcon()
+            nhentaiUI.layout = null
+            nhentaiUI.isVisible = visible
 
+            urlFieldComponent.setURLField(nhentaiUI, urlField)
+        }
         closingWindow(nhentaiUI = nhentaiUI)
     }
 
