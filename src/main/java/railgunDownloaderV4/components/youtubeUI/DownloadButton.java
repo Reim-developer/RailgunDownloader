dd8161b20e5143fe3c235fec8c1fc3dcb348cd4d
Reim-developer <contact.kaxtr@gmail.com>
2025-01-09 22:59:02 +0700

    Optimized & clean code

--- src/main/java/railgunDownloaderV4/components/youtubeUI/DownloadButton.java
+++ src/main/java/railgunDownloaderV4/components/youtubeUI/DownloadButton.java
@@ -3,26 +3,37 @@
  * DownloadButton.java
  */
 package railgunDownloaderV4.components.youtubeUI;
+import railgunDownloaderV4.components.ulti.DirExists;
+import railgunDownloaderV4.components.ulti.MatchURL;
+import railgunDownloaderV4.components.ulti.MessageDialog;
+import railgunDownloaderV4.components.ulti.SetIconButton;
 import railgunDownloaderV4.components.youtubeUI.helper.DownloadHelper;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Objects;
-import java.io.File;
 
 public class DownloadButton {
     private final YoutubeUI YoutubeUIContext;
     private final DownloadHelper Helper;
+    private final MessageDialog messageDialog;
+    private final SetIconButton setIconButton;
+    private final MatchURL matchURL;
+    private final DirExists dirExists;
 
     public DownloadButton(YoutubeUI YoutubeUIContext) {
         this.YoutubeUIContext = YoutubeUIContext;
         Helper = new DownloadHelper();
+        messageDialog = new MessageDialog();
+        setIconButton = new SetIconButton();
+        matchURL = new MatchURL();
+        dirExists = new DirExists();
     }
     public void SetDownloadButton(JFrame App, JButton DownloadButton) {
-        Image ButtonIcon = Toolkit.getDefaultToolkit().getImage(
-                Objects.requireNonNull(getClass().getResource("/Download.png"))
+        setIconButton.setIcon(
+                DownloadButton,
+                "/Download.png",
+                60, 60
         );
         DownloadButton.setSize(new Dimension(60, 60));
-        DownloadButton.setIcon(new ImageIcon(ButtonIcon.getScaledInstance(60, 60, Image.SCALE_FAST)));
         DownloadButton.setBorder(null);
         DownloadButton.setFocusPainted(false);
         DownloadButton.setBorderPainted(false);
@@ -30,7 +41,6 @@ public class DownloadButton {
         DownloadButton.setLocation(new Point(200, 415));
 
         DownloadButton.setToolTipText("Download video");
-
         Download(DownloadButton);
 
         App.add(DownloadButton);
@@ -42,24 +52,18 @@ public class DownloadButton {
             String SavePathValue = YoutubeUIContext.ShowPathField.getText();
             String ChooseValue = YoutubeUIContext.QualityListBox.getSelectedValue();
 
-            File SaveFolder = new File(SavePathValue);
-
-            if(InputURLValue.isEmpty()) {
-                JOptionPane.showMessageDialog(
-                        null,
-                        "Please input your Youtube video URL",
-                        "Notification",
-                        JOptionPane.INFORMATION_MESSAGE
+            if(!matchURL.matchURL(InputURLValue)) {
+                messageDialog.showMessageNotification(
+                        "Please input valid Youtube video URL",
+                        "notification"
                 );
                 return;
             }
 
-            if(!SaveFolder.exists() && !SaveFolder.isDirectory()) {
-                JOptionPane.showMessageDialog(
-                        null,
+            if(!dirExists.checkDirExists(SavePathValue)) {
+                messageDialog.showMessageNotification(
                         "Your save folder is not exists. Please try again",
-                        "Notification",
-                        JOptionPane.INFORMATION_MESSAGE
+                        "Notification"
                 );
                 return;
             }
