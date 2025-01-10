6f213b9af691790e168558a669c3cf1b13876f7b
Reim-developer <contact.kaxtr@gmail.com>
2025-01-10 18:32:04 +0700

    Added new UI class

---
+++ src/main/java/railgunDownloaderV4/components/facebookUI/InputPath.kt
@@ -0,0 +1,26 @@
+/*
+ * RAILGUN DOWNLOADER - VERSION 4.0.0
+ * InputPath.kt
+ */
+package railgunDownloaderV4.components.facebookUI
+
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.Point
+import javax.swing.JFrame
+import javax.swing.JTextField
+
+class InputPath {
+    fun setInputPath(app: JFrame, inputPathField: JTextField) {
+        inputPathField.apply {
+            size = Dimension(400, 30)
+            location = Point(100, 350)
+            isEditable = false
+            border = null
+            background = Color(69, 69, 69)
+            foreground = Color.WHITE
+        }
+
+        app.add(inputPathField)
+    }
+}
