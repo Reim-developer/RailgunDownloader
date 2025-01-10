155f7d2de0ea4a17eb62661610f7549caef5b3b4
Reim-developer <contact.kaxtr@gmail.com>
2025-01-10 21:30:30 +0700

    Added new event for 'download' UI component

---
+++ src/main/java/railgunDownloaderV4/components/facebookUI/events/Download.kt
@@ -0,0 +1,40 @@
+/*
+ * RAILGUN DOWNLOADER - VERSION 4.0.0
+ * Download.kt
+ */
+package railgunDownloaderV4.components.facebookUI.events
+
+import railgunDownloaderV4.components.facebookUI.FacebookUI
+import railgunDownloaderV4.components.ulti.ClearEvents
+import railgunDownloaderV4.components.ulti.DirExists
+import railgunDownloaderV4.components.ulti.MatchURL
+import railgunDownloaderV4.components.ulti.MessageDialog
+import javax.swing.JButton
+
+class Download (private val facebookUI: FacebookUI){
+
+    private val clearEvents: ClearEvents by lazy { ClearEvents() }
+    private val matchURL: MatchURL by lazy { MatchURL() }
+    private val dirExists: DirExists by lazy { DirExists() }
+    private val messageDialog: MessageDialog by lazy {  MessageDialog() }
+
+    fun setDownload(downloadButton: JButton) {
+
+        clearEvents.clearActionListeners(downloadButton)
+
+        downloadButton.addActionListener {
+            val urlField = facebookUI.inputURLField.text
+            val pathField = facebookUI.inputPathField.text
+
+            matchURL.takeIf { !it.matchURL(urlField) }?.let {
+                messageDialog.showMessageNotification("Invalid URL. Please try again")
+                return@addActionListener
+            }
+
+            dirExists.takeIf { !it.checkDirExists(pathField) }?.let {
+                messageDialog.showMessageNotification("Invalid directory. Please try again")
+                return@addActionListener
+            }
+        }
+    }
+}
