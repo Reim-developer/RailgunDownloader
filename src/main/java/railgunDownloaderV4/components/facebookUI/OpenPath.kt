13688103ae2436bbf08c595d01065ccd41de1d12
Reim-developer <contact.kaxtr@gmail.com>
2025-01-10 19:52:10 +0700

    Added new UI component 'openSavePath'

---
+++ src/main/java/railgunDownloaderV4/components/facebookUI/OpenPath.kt
@@ -0,0 +1,39 @@
+/*
+ * RAILGUN DOWNLOADER - VERSION 4.0.0
+ * OpenPath.kt
+ */
+package railgunDownloaderV4.components.facebookUI
+
+import railgunDownloaderV4.components.ulti.PathDialog
+import railgunDownloaderV4.components.ulti.SetIconButton
+import java.awt.Dimension
+import java.awt.Point
+import javax.swing.JButton
+import javax.swing.JFrame
+
+class OpenPath (private val facebookUI: FacebookUI){
+
+    private val setIconButton: SetIconButton by lazy { SetIconButton() }
+    private val pathDialog: PathDialog by lazy { PathDialog() }
+
+    fun setOpenPath(app: JFrame, openSavePath: JButton) {
+        openSavePath.apply {
+            size = Dimension(50, 50)
+            isContentAreaFilled = false
+            border = null
+            isFocusPainted = false
+            isBorderPainted = false
+            location = Point(100, 440)
+            setIconButton.setIcon(
+                openSavePath, "/FolderPath.png",
+                50, 50
+            )
+        }
+
+        pathDialog.setShowDialog(
+            openSavePath, "Choose save video facebook directory",
+            facebookUI.inputPathField
+        )
+        app.add(openSavePath)
+    }
+}
