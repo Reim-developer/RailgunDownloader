130d4b453044b1a6a675275bc5b4d0458bb5c0ea
Reim-developer <contact.kaxtr@gmail.com>
2025-01-13 15:05:20 +0700

    Added Download Kotlin class

---
+++ src/main/kotlin/railgunDownloaderV4/components/facebookUI/events/Download.kt
@@ -0,0 +1,51 @@
+/*
+ * RAILGUN DOWNLOADER - VERSION 4.0.0
+ * File: Download.kt
+ * This project is licensed GPL-3.0 License.
+ * LICENSE: https://www.gnu.org/licenses/gpl-3.0.html
+ * Contribution: Reim
+ */
+package railgunDownloaderV4.components.facebookUI.events
+import railgunDownloaderV4.components.facebookUI.FacebookUI
+import railgunDownloaderV4.components.facebookUI.helper.FacebookDownloadHelper
+import railgunDownloaderV4.components.ulti.ClearEvents
+import railgunDownloaderV4.components.ulti.DirExists
+import railgunDownloaderV4.components.ulti.MatchURL
+import railgunDownloaderV4.components.ulti.MessageDialog
+import javax.swing.JButton
+
+
+class Download (private val facebookUI: FacebookUI){
+
+    private val clearEvents: ClearEvents by lazy { ClearEvents() }
+    private val matchURL: MatchURL by lazy { MatchURL() }
+    private val dirExists: DirExists by lazy { DirExists() }
+    private val messageDialog: MessageDialog by lazy {  MessageDialog() }
+    private val facebookDownloadHelper: FacebookDownloadHelper by lazy { FacebookDownloadHelper() }
+
+    fun setDownload(downloadButton: JButton) {
+
+        clearEvents.clearActionListeners(downloadButton)
+
+        downloadButton.addActionListener {
+            val urlField = facebookUI.inputURLField.text
+            val pathField = facebookUI.inputPathField.text
+            val videoQuality = facebookUI.qualityListBox.selectedValue
+
+            matchURL.takeIf { !it.matchURL(urlField) }?.let {
+                messageDialog.showMessageNotification("Invalid URL. Please try again")
+                return@addActionListener
+            }
+
+            dirExists.takeIf { !it.checkDirExists(pathField) }?.let {
+                messageDialog.showMessageNotification("Invalid directory. Please try again")
+                return@addActionListener
+            }
+
+            facebookDownloadHelper.start(
+                "bin/FacebookHelper/FacebookHelper.lib", pathField,
+                urlField, videoQuality, facebookUI.logResultArea
+            )
+        }
+    }
+}
