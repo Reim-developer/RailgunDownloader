5b7154213ea817320504e1d7cc3d14985f7a2a97
Reim-developer <contact.kaxtr@gmail.com>
2025-01-13 20:48:55 +0700

    Added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+/*
+ * RAILGUN DOWNLOADER - VERSION 4.0.0
+ * File: build.gradle.kts
+ * This project is licensed GPL-3.0 License.
+ * LICENSE: https://www.gnu.org/licenses/gpl-3.0.html
+ * Contribution: Reim
+ */
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.8.0"
 }
@@ -16,6 +23,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jsoup:jsoup:1.18.3")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
 kotlin {
     jvmToolchain(17)
