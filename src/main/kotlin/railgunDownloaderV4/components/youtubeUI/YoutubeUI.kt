6dbf157e71251df602cf11daac9eb00b7fbaecc8
Reim-developer <contact.kaxtr@gmail.com>
2025-01-17 22:10:57 +0700

    Fixed: + Log result area display issus

--- src/main/kotlin/railgunDownloaderV4/components/youtubeUI/YoutubeUI.kt
+++ src/main/kotlin/railgunDownloaderV4/components/youtubeUI/YoutubeUI.kt
@@ -8,24 +8,24 @@
 package railgunDownloaderV4.components.youtubeUI
 
 import railgunDownloaderV4.Application
+import railgunDownloaderV4.components.global.LogResult
 import railgunDownloaderV4.components.ulti.SetIcon
 import java.awt.Color
 import java.awt.Dimension
+import java.awt.Point
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
 import javax.swing.*
 
 class YoutubeUI(val appScene: Application) {
-
-    private val app: JFrame by lazy { JFrame() }
     private val inputURLField: InputURL by lazy { InputURL() }
     private val showPathField: ShowPath by lazy { ShowPath() }
     private val choosePath: ChoosePath by lazy { ChoosePath(this) }
     private val downloadButton = DownloadButton(this)
-    private val logResult: Resultlog by lazy { Resultlog() }
+    private val logResult: LogResult by lazy { LogResult() }
     private val listQuality: QualityList by lazy { QualityList() }
     private val setIcon: SetIcon by lazy { SetIcon() }
-    private val cancelDownload: CancelDownload by lazy { CancelDownload()}
+    private val cancelDownload: CancelDownload by lazy { CancelDownload() }
 
     val urlField: JTextField by lazy { JTextField() }
     val pathField: JTextField by lazy { JTextField() }
@@ -36,32 +36,35 @@ class YoutubeUI(val appScene: Application) {
     private val cancelDownloadButton by lazy { JButton() }
 
     fun showYoutubeUI(visible: Boolean) {
+        val app = JFrame()
+        app.title = "Download from Youtube"
+        app.iconImage = setIcon.setAppIcon()
+        app.size = Dimension(600, 650)
+        app.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
+        app.setLocationRelativeTo(null)
+        app.layout = null
+        app.isResizable = false
+        app.maximumSize = Dimension(600, 650)
+        app.contentPane.background = Color(21, 21, 21)
+        app.isVisible = visible
 
-        SwingUtilities.invokeLater {
-            app.title = "Download from Youtube"
-            app.iconImage = setIcon.setAppIcon()
-            app.size = Dimension(600, 650)
-            app.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
-            app.setLocationRelativeTo(null)
-            app.layout = null
-            app.isResizable = false
-            app.maximumSize = Dimension(600, 650)
-            app.contentPane.background = Color(21, 21, 21)
-            app.isVisible = visible
+        inputURLField.setInputURL(app, urlField)
+        showPathField.setShowPath(app, pathField)
+        choosePath.setChoosePath(app, choosePathButton)
+        downloadButton.setDownloadButton(app, buttonDownload)
+        logResult.setLogResult(
+            app, resultLog,
+            Dimension(400, 200), Point(100, 50),
+            "Your download result will be displayed here"
+        )
+        listQuality.setQuality(app, qualityBox)
+        cancelDownload.setCancelDownload(app, cancelDownloadButton)
 
-            inputURLField.setInputURL(app, urlField)
-            showPathField.setShowPath(app, pathField)
-            choosePath.setChoosePath(app, choosePathButton)
-            downloadButton.setDownloadButton(app, buttonDownload)
-            logResult.setResultlog(app, resultLog)
-            listQuality.setQuality(app, qualityBox)
-            cancelDownload.setCancelDownload(app, cancelDownloadButton)
-            onClosing()
-        }
+        onClosing(app)
     }
 
-    private fun onClosing() {
-        app.addWindowListener(object : WindowAdapter() {
+    private fun onClosing(appTarget: JFrame) {
+        appTarget.addWindowListener(object : WindowAdapter() {
             override fun windowClosing(e: WindowEvent) {
                 appScene.app.isVisible = true
             }
